009be891f878139d6243fe04efc43f0398c795f6
Francisco Santana Cardoso <fsantanagy@gmail.com>
2023-01-09 20:41:17 -0300

    start repository

---
+++ whatsAppClone/whatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsAppCloneApp.swift
+//  whatsAppClone
+//
+//  Created by Francisco Santana Cardoso on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct whatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
